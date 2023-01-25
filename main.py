1ae4e222c62326aade1d53fcb0d666ad7d697f49
kronic5179 <98897386+Allen-Saunders@users.noreply.github.com>
2023-01-25 18:20:46 +0000

    ahhhh

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def print_menu():
   print("AllenSaundersWordleProject")
   print("Type A 5 Letter Word, Press Enter When Fully Typed.")
+
+def read_random_word():
+  with open("words.txt") as f:
+    words = f.read().splitlines()
+
 print_menu()
